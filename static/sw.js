541f8bc0cc490791e6b2455ce0912b794aff63d5
sukumar <139058073+sukumar1111@users.noreply.github.com>
2025-02-24 22:16:13 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,42 +1,36 @@
 const CACHE_NAME = "tomato-bot-cache-v1";
 const urlsToCache = [
     "/",
-    "/offline.html",  // Add an offline page
     "/static/style.css",
+    "/static/img1.jpg",
     "/static/manifest.json",
-    "/static/icons/icon-192x192.png",
-    "/static/icons/icon-512x512.png"
+    "/static/offline.html"
 ];
 
-// Install Service Worker and cache resources
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-            .then(self.skipWaiting())
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
-// Activate Service Worker and remove old caches
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
+
 self.addEventListener("activate", (event) => {
     event.waitUntil(
-        caches.keys().then((cacheNames) =>
-            Promise.all(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
                 cacheNames.map((cache) => {
                     if (cache !== CACHE_NAME) {
                         return caches.delete(cache);
                     }
                 })
-            )
-        )
-    );
-});
-
-// Fetch resources from cache or show offline page
-self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request).catch(() => caches.match("/offline.html"));
+            );
         })
     );
 });
